c44964bc4b0835e181d1f6d1413bd606c27325e9
bonekyaw <phonenai2014@gmail.com>
2025-05-09 15:22:57 +0630

    Layout - position

--- my-shop/app/(tabs)/index.tsx
+++ my-shop/app/(tabs)/index.tsx
@@ -1,39 +1,39 @@
-import { StyleSheet, View, Text } from "react-native";
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        flexDirection: "row",
-        flexWrap: "wrap",
-        gap: 10,
-        // alignItems: "center",
-        // justifyContent: "center",
-      }}
-    >
+    <SafeAreaView style={{ flex: 1 }}>
       <View
         style={{
-          width: 100,
-          height: 100,
+          position: "static",
+          top: 0,
+          left: 20,
+          width: 150,
+          height: 150,
           backgroundColor: "#CA64E3",
         }}
       />
-      <View style={{ width: 100, height: 100, backgroundColor: "#CA00e3" }} />
       <View
         style={{
-          width: 100,
-          height: 100,
+          position: "static",
+          top: 20,
+          left: 40,
+          width: 150,
+          height: 150,
           backgroundColor: "#0A00e3",
-          // alignSelf: "flex-end",
         }}
       />
-      <View style={{ width: 100, height: 100, backgroundColor: "orange" }} />
-      <View style={{ width: 100, height: 100, backgroundColor: "purple" }} />
-      <View style={{ width: 100, height: 100, backgroundColor: "indigo" }} />
-      <View style={{ width: 100, height: 100, backgroundColor: "yellow" }} />
-      <View style={{ width: 100, height: 100, backgroundColor: "red" }} />
+      <View
+        style={{
+          position: "static",
+          top: 40,
+          right: 0,
+          width: 150,
+          height: 150,
+          backgroundColor: "#0Aeeff",
+        }}
+      />
     </SafeAreaView>
   );
 }
