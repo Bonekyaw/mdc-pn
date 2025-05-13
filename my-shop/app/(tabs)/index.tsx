aa5a474fb71c9c826fe876cae1e182b30ed06854
bonekyaw <phonenai2014@gmail.com>
2025-05-13 18:32:59 +0630

    Fetching api

--- my-shop/app/(tabs)/index.tsx
+++ my-shop/app/(tabs)/index.tsx
@@ -1,49 +1,61 @@
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, StyleSheet, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+type User = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+};
+
 export default function HomeScreen() {
+  const [user, setUser] = useState<User | null>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://reqres.in/api/users/2", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        console.log(response);
+        return null;
+      }
+
+      const json = await response.json();
+      setUser(json.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View
-        style={{
-          position: "static",
-          top: 0,
-          left: 20,
-          width: 150,
-          height: 150,
-          backgroundColor: "#CA64E3",
-        }}
-      />
-      <View
-        style={{
-          position: "static",
-          top: 20,
-          left: 40,
-          width: 150,
-          height: 150,
-          backgroundColor: "#0A00e3",
-        }}
-      />
-      <View
-        style={{
-          position: "static",
-          top: 40,
-          right: 0,
-          width: 150,
-          height: 150,
-          backgroundColor: "#0Aeeff",
-        }}
-      />
+      <Text style={styles.text}>Hello Networking</Text>
+      {isLoading && <ActivityIndicator />}
+      {user ? <Text>{user?.email}</Text> : <Text>No User Found</Text>}
     </SafeAreaView>
   );
 }
 
-// const styles = StyleSheet.create({
-//   text: {
-//     marginTop: 50,
-//     color: "#CA64E3",
-//     fontSize: 30,
-//     fontWeight: "bold",
-//     textAlign: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  text: {
+    marginTop: 50,
+    color: "#CA64E3",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
